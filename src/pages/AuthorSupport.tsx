9f6d17f6112cd58c65e2bf98cbb862731978c3f6
l1safe <lisafe2fae@gmail.com>
2024-05-01 20:17:55 +0300

    donation_page

---
+++ src/pages/AuthorSupport.tsx
@@ -0,0 +1,9 @@
+
+import React,{FC} from 'react'
+import Donation from '../components/AuthorSupport'
+
+const AuthorSupportPage : FC = () => {
+  return <Donation/>
+}
+
+export default AuthorSupportPage
---
+++ src/pages/AuthorSupportFish.tsx
@@ -0,0 +1,9 @@
+
+import React,{FC} from 'react'
+import Donation from '../components/AuthorSupportFish'
+
+const AuthorSupportFishPage : FC = () => {
+  return <Donation/>
+}
+
+export default AuthorSupportFishPage
